fe0ae5780791ee17461142f89e988cd717f3e852
Troy Good <troygood13@gmail.com>
2020-01-21 13:26:47 -0800

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Troy Good on 1/20/20.
+//  Copyright © 2020 tgood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
