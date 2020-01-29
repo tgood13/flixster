8f6cfe53a484a0ca74f6f794058af1f9c394dda2
Troy Good <troygood13@gmail.com>
2020-01-28 17:50:16 -0800

    part2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Troy Good on 1/28/20.
+//  Copyright © 2020 tgood. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+  @IBOutlet weak var posterView: UIImageView!
+}
